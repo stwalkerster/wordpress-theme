0815ab7a576bbc794e6024d924dd6023b6eb2f37
Simon Walker <github@stwalkerster.co.uk>
2012-07-26 00:40:49 +0100

    register headers

--- functions.php
+++ functions.php
@@ -10,7 +10,50 @@ register_sidebar( array(
 		'after_title' => '</h3>',
 	) );
 
-
+register_default_headers( array(
+	'disk' => array(
+		'url' => '%s/images/header/Disque_dur_0007.jpg',
+		'thumbnail_url' => '%s/images/header/thumbs/Disque_dur_0007.jpg',
+		'description' => 'Disk head'
+	),
+	'plasma' => array(
+		'url' => '%s/images/header/Plasma-lamp.jpg',
+		'thumbnail_url' => '%s/images/header/thumbs/Plasma-lamp.jpg',
+		'description' => 'Plasma lamp'
+	),
+	'lights' => array(
+		'url' => '%s/images/header/Light_painting_gnangarra-1.jpg',
+		'thumbnail_url' => '%s/images/header/thumbs/Light_painting_gnangarra-1.jpg',
+		'description' => 'Light painting'
+	),
+	'pencils' => array(
+		'url' => '%s/images/header/Colouring_pencils.jpg',
+		'thumbnail_url' => '%s/images/header/thumbs/Colouring_pencils.jpg',
+		'description' => 'Colouring pencils'
+	),
+	'sparkler' => array(
+		'url' => '%s/images/header/Sparkler.jpg',
+		'thumbnail_url' => '%s/images/header/thumbs/Sparkler.jpg',
+		'description' => 'Sparkler'
+	),
+	'balloon' => array(
+		'url' => '%s/images/header/Cappadocia_Balloon_Inflating_Wikimedia_Commons.jpg',
+		'thumbnail_url' => '%s/images/header/thumbs/Cappadocia_Balloon_Inflating_Wikimedia_Commons.jpg',
+		'description' => 'Hot air balloon'
+	),
+	'beach' => array(
+		'url' => '%s/images/header/Lanzarote_3_Luc_Viatour.jpg',
+		'thumbnail_url' => '%s/images/header/thumbs/Lanzarote_3_Luc_Viatour.jpg',
+		'description' => 'Lanzarote beach'
+	),
+	'lightning' => array(
+		'url' => '%s/images/header/Blitze_IMGP6376_wp.jpg',
+		'thumbnail_url' => '%s/images/header/thumbs/Blitze_IMGP6376_wp.jpg',
+		'description' => 'Lightning'
+	),
+) );
+	
+	
 add_filter('body_class', 'adjust_body_class', 20, 2);
 
 function adjust_body_class($wp_classes, $extra_classes) {
