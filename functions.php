468bf88177ca5f4a435ca5dccd0dc3debb782719
Simon Walker <github@stwalkerster.co.uk>
2012-07-25 21:22:25 +0000

    fix

--- functions.php
+++ functions.php
@@ -9,3 +9,20 @@ register_sidebar( array(
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>',
 	) );
+
+
+add_filter('body_class', 'adjust_body_class', 20, 2);
+
+function adjust_body_class($wp_classes, $extra_classes) {
+	if( is_page_template('page-alt.php') ) {
+		// Filter the body classes
+		foreach($wp_classes as $key => $value) {
+			if ($value == 'singular') {
+				unset($wp_classes[$key]);
+			}
+		}
+	}
+
+	// Add the extra classes back untouched
+	return array_merge($wp_classes, (array) $extra_classes );
+}
