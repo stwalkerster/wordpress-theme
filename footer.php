7c54afbff8a650a8515cffc92487ea04faf9e5f4
Simon Walker <github@stwalkerster.co.uk>
2014-04-22 02:44:33 +0100

    spacing

--- footer.php
+++ footer.php
@@ -6,9 +6,8 @@
  */
 ?>
 
-	</div><!-- #main -->
-
-	<footer id="colophon" role="contentinfo">
+		</div><!-- #main -->
+		<footer id="colophon" role="contentinfo">
 
 			<?php
 				/* A sidebar in the footer? Yep. You can can customize
@@ -20,14 +19,13 @@
 
 			<div id="site-generator">
 				<?php if( ! dynamic_sidebar ( "sidebar-footer" ) ) : ?>
-					<?php do_action( 'twentyeleven_credits' ); ?>
-					<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
+				<?php do_action( 'twentyeleven_credits' ); ?>
+				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
 				<?php endif; ?>
 			</div>
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
 
-<?php wp_footer(); ?>
-
+	<?php wp_footer(); ?>
 </body>
 </html>
