f55170743879447d55e3c8ba890c4849390ce0da
Simon Walker <simon@stwalkerster.co.uk>
2014-04-28 01:32:06 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -21,6 +21,16 @@ register_sidebar( array(
 	) );
 	
 register_sidebar( array(
+		'name' => 'Ingress Sidebar',
+		'id' => 'sidebar-ingress',
+		'description' => 'The sidebar for the ingress page template',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => "</aside>",
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+	
+register_sidebar( array(
 		'name' => 'Footer area',
 		'id' => 'sidebar-footer',
 		'description' => 'The footer widget area',
@@ -189,7 +199,7 @@ endif; // twentyeleven_setup
 add_filter('body_class', 'adjust_body_class', 20, 2);
 
 function adjust_body_class($wp_classes, $extra_classes) {
-	if( is_page_template('page-alt.php') || is_page_template('page-eventscrew.php')) {
+	if( is_page_template('page-alt.php') || is_page_template('page-eventscrew.php') || is_page_template('page-ingress.php')) {
 		// Filter the body classes
 		foreach($wp_classes as $key => $value) {
 			if ($value == 'singular') {
